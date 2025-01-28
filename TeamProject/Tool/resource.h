7b6c643fad5ea8f792acecd393c4c64427ddbfee
YUgyeong-Choi <cyk7800@naver.com>
2025-01-28 19:06:01 +0900

    애니메이션 관련 버튼 추가

--- TeamProject/Tool/resource.h
+++ TeamProject/Tool/resource.h
@@ -68,6 +68,7 @@
 #define IDC_SLIDER12                    1051
 #define IDC_SLIDER15                    1052
 #define IDC_BUTTON9                     1053
+#define IDC_RADIO4                      1053
 #define IDC_BUTTON8                     1054
 #define IDC_PICTURE2                    1055
 #define IDC_EDIT14                      1056
@@ -80,7 +81,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        315
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1052
+#define _APS_NEXT_CONTROL_VALUE         1054
 #define _APS_NEXT_SYMED_VALUE           314
 #endif
 #endif
